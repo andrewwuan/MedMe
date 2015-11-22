03e1912cc75b91583dd5cde36a08e08b468700c9
An Wu <awu@comprehend.com>
2015-11-22 11:43:21 -0800

    added dummy hospitals and drop markers

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,99 @@ angular.module('MedMe', [
   $routeProvider.when('/search', {templateUrl:'search.html', reloadOnSearch: false});
 });
 
+var hospitals = [
+  {"ST MARY'S MEDICAL CENTER": {
+    "wait_time": "14",
+    "phone": "415-668-1000",
+    "address": "450 Stanyan St, San Francisco, CA 94117",
+    "lat": "37.77416",
+    "lon": "-122.45359"
+  }},
+  {"CHINESE HOSPITAL": {
+    "wait_time": "20",
+    "phone": "415-982-2400",
+    "address": "845 Jackson St, San Francisco, CA 94117",
+    "lat": "37.79552",
+    "lon": "-122.40919"
+  }},
+  {"CALIFORNIA PACIFIC MEDICAL CTR-DAVIES CAMPUS HOSP": {
+    "wait_time": "21",
+    "phone": "415-600-6000",
+    "address": "601 Duboce Ave, San Francisco, CA 94117",
+    "lat": "37.76824",
+    "lon": "-122.43459"
+  }},
+  {"CALIFORNIA PACIFIC MEDICAL CTR-PACIFIC CAMPUS HOSP": {
+    "wait_time": "24",
+    "phone": "415-600-6000",
+    "address": "2333 Buchanan Street, San Francisco, CA 94117",
+    "lat": "37.79084",
+    "lon": "-122.43127"
+  }},
+  {"CALIFORNIA PACIFIC MEDICAL CTR - ST. LUKE'S CAMPUS": {
+    "wait_time": "38",
+    "phone": "415-641-6562",
+    "address": "3555 Cesar Chavez Street, San Francisco, CA 94117",
+    "lat": "37.74766",
+    "lon": "-122.42070"
+  }}
+]
+
 var map;
 function initMap() {
   // Create a map object and specify the DOM element for display.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 37.779206, lng: -122.396250},
     scrollwheel: false,
-    zoom: 8
+    zoom: 12
+  });
+
+  var container = document.getElementById("search-list-container");
+
+  for (var i = 0; i < hospitals.length; i++) {
+    var hospital = hospitals[i];
+
+    for (var hospitalName in hospital) {
+      // Drop markers on the map
+      addMarker({lat: parseFloat(hospital[hospitalName].lat), lng: parseFloat(hospital[hospitalName].lon)},
+        map, (i+1).toString());
+
+      // Add list items to the page
+      var hospitalNode = document.createElement("div");
+      hospitalNode.setAttribute("class", "list-group-item list-group-item-home");
+
+      var waitTimeNode = document.createElement("div");
+      var waitTimeText = document.createElement("i");
+      waitTimeText.setAttribute("class", "text-primary");
+      waitTimeText.textContent = hospital[hospitalName].wait_time + " minutes";
+      waitTimeNode.appendChild(waitTimeText);
+      hospitalNode.appendChild(waitTimeNode);
+
+      var hospitalInfoNode = document.createElement("div");
+      var hospitalNameText = document.createElement("h3");
+      hospitalNameText.setAttribute("class", "home-heading center-block hospital-name");
+      hospitalNameText.textContent = (i+1) + ". " + hospitalName;
+      hospitalInfoNode.appendChild(hospitalNameText);
+      var hospitalPhone = document.createElement("p");
+      hospitalPhone.textContent = hospital[hospitalName].phone;
+      hospitalInfoNode.appendChild(hospitalPhone);
+      var hospitalAddress = document.createElement("p");
+      hospitalAddress.textContent = hospital[hospitalName].address;
+      hospitalInfoNode.appendChild(hospitalAddress);
+      hospitalNode.appendChild(hospitalInfoNode);
+
+      container.appendChild(hospitalNode);
+    }
+  }
+}
+
+// Adds a marker to the map.
+function addMarker(location, map, label) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: label,
+    map: map
   });
 }
